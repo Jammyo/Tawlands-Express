bfda16e6f05cb48d6b58aa7b659a7d91e1c00577
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2020-05-02 19:48:53 +0100

    Add camera following and a carriage to the train.

---
+++ Assets/Tawlands/Train/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _train;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_train.position.x, _train.position.y, transform.position.z);
+    }
+}
