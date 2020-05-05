d7997188f9ab63e481f8960356f94e746fe74427
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2020-05-05 17:20:19 +0100

    Add a player scene so I can play around with character animations. Add a placeholder face, body, and legs while I figure out how artisting works.. Add movement and jumping, and some blocks to jump on to make it more interesting.

--- Assets/Prototypes/TrainSystems/Engine/Engine.cs
+++ Assets/Prototypes/TrainSystems/Engine/Engine.cs
@@ -4,16 +4,5 @@ namespace Prototypes.TrainSystems.Engine
 {
     public class Engine : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
---
+++ Assets/Tawlands/Player/Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _jumpSpeed;
+
+    private float _verticalSpeed;
+    
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var jump = Input.GetButton("Jump");
+
+        var direction = _maxSpeed * new Vector3(horizontal, 0, vertical).normalized;
+        if (_characterController.isGrounded)
+        {
+            _verticalSpeed = jump && _characterController.isGrounded ? _jumpSpeed : 0;
+        }
+        _verticalSpeed -= 20 * Time.deltaTime;
+        var collisionFlags = _characterController.Move(Time.deltaTime * new Vector3(direction.x, _verticalSpeed, direction.z));
+    }
+}
