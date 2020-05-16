78af2146b6fa4f7792a4898a4b8ee2f0f38b7149
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2020-05-16 08:33:19 +0100

    Created a new train with far less physics to improve the feel of it. Changed the Handle script to use the new changes (now much simpler). Added an animation for the handle bar being pushed on each side.

---
+++ Assets/Tawlands/Train/HandCab/Component/Handle/HandBar.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class HandBar : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private MeshRenderer _meshRenderer;
+
+    private Rigidbody _accelerationBody;
+
+    [SerializeField] private float _speed;
+    [SerializeField] private string _barName;
+
+    private const float GreenHue = 120f / 360;
+    private bool _isPlayerTouching;
+
+    private void Awake()
+    {
+        _accelerationBody = transform.parent.GetComponentInParent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _meshRenderer.material.color = Color.HSVToRGB(GreenHue, 1, 1);
+            _isPlayerTouching = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            _meshRenderer.material.color = Color.HSVToRGB(GreenHue, 0, 1);
+            _isPlayerTouching = false;
+        }
+    }
+
+    private float _previousArmAngle;
+    
+    private void Update()
+    {
+        if (_isPlayerTouching && Input.GetKeyDown(KeyCode.E))
+        {
+            _animator.SetTrigger($"{_barName} Handle Down");
+        }
+
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName($"{_barName} Handle Down"))
+        {
+            _accelerationBody.AddRelativeForce(_speed * Time.deltaTime * Vector3.left);
+        }
+    }
+}
