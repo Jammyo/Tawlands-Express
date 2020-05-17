5d43fbd57c091a15a9804f5fa443ea4548dbd020
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2020-05-17 22:36:11 +0100

    Allow the player to move with the train, just need to figure out why the train gets stuck on the player.

--- Assets/Tawlands/Player/Controller.cs
+++ Assets/Tawlands/Player/Controller.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Tawlands.Player
 {
@@ -9,7 +11,19 @@ namespace Tawlands.Player
         [SerializeField] private float _jumpSpeed;
 
         private float _verticalSpeed;
-    
+
+        private List<GameObject> _activeCollisions = new List<GameObject>();
+
+        private void OnCollisionEnter(Collision other)
+        {
+            _activeCollisions.Add(other.gameObject);
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            _activeCollisions.Remove(other.gameObject);
+        }
+
         void Update()
         {
             var horizontal = Input.GetAxisRaw("Horizontal");
@@ -22,6 +36,20 @@ namespace Tawlands.Player
                 _verticalSpeed = jump && _characterController.isGrounded ? _jumpSpeed : 0;
             }
             _verticalSpeed -= 20 * Time.deltaTime;
+
+            if (_characterController.isGrounded && !jump)
+            {
+                var train = _activeCollisions.FirstOrDefault(o => o.CompareTag("Train"));
+                if(train != null)
+                {
+                    var rigidbody = train.GetComponent<Rigidbody>();
+                    if (rigidbody != null)
+                    {
+                        direction += rigidbody.velocity;
+                    }
+                }
+            }
+            
             var collisionFlags = _characterController.Move(Time.deltaTime * new Vector3(direction.x, _verticalSpeed, direction.z));
         }
     }
